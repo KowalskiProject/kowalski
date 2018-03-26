c6fdbd703257666590e6cafb15c07dd68a3a0911
Fabio Dassan dos Santos <fabiodassan@gmail.com>
2018-03-26 15:13:58 -0300

    Change column type to text
    Edits description type to be text instead of varchar.

--- src/main/java/com/app/kowalski/da/entities/Project.java
+++ src/main/java/com/app/kowalski/da/entities/Project.java
@@ -9,12 +9,14 @@ import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -36,7 +38,11 @@ public class Project {
 
 	@NotNull
 	private String code;
+
+	@Lob
+	@Column
 	private String description;
+
 	private Date startDate;
 	private Date endDate;
 
